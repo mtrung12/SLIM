a7494d895759b331803138ef78c9f83e66962069
Ngô Minh Trung <trung.nm226004@sis.hust.edu.vn>
2025-10-29 13:28:59 +0700

    add arg autotune

--- auto_tune_file.sh
+++ auto_tune_file.sh
@@ -5,12 +5,22 @@ for lr in 1e-5 3e-5 5e-5; do
     for wd in 0.0 0.01; do
       for slot in 0.5 1.0 2.0; do
         python main.py \
-          --task mytask \
-          --model_dir model_lr${lr}_bs${bs}_wd${wd}_slot${slot} \
+          --task phoatis_word \
+          --model_type bert \
+          --model_dir phoatis_model_lr${lr}_bs${bs}_wd${wd}_slot${slot} \
           --do_train --do_eval \
           --learning_rate $lr \
           --train_batch_size $bs \
           --weight_decay $wd \
+          --multi_intent 0 \
+          --intent_seq 0 \
+          --tag_intent 1 \
+          --BI_tag 1 \
+          --intent_attn 1 \
+          --cls_token_cat 1 \
+          --num_mask 6 \
+          --patience 0 \
+          --seed 12 \
           --slot_loss_coef $slot
       done
     done
